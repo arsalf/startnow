865811ed792631141a688e90b5261e1c9e141c9c
zidanalba <zmuhammadzidan@gmail.com>
2022-05-13 14:23:02 +0700

    Nambah Logo

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Startnow</a>
+        <a class="navbar-brand" href="#">
+        <img src="{{asset('image/logo-start-now-purple.png')}}" alt="..." height="36">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             {{-- <span class="navbar-toggler-icon"></span> --}}
